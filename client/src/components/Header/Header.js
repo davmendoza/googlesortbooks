08d6a4b272274a1f55a978bd199914840b72c39d
david mendoza <davidsmacbook@davids-mbp.attlocal.net>
2020-12-02 18:49:54 -0800

    new files

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return(
+        <header className="header">
+            <i class="fas fa-book"></i>
+            <h1 className="header__h1">Book Cards</h1>
+
+        </header>
+    )
+}
+
+export default Header;

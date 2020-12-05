144c445734547d91db17ea3e1052290790d9a20e
david mendoza <davidsmacbook@davids-mbp.attlocal.net>
2020-12-05 11:11:25 -0800

    added descripion

--- client/src/components/BookCard/BookCard.js
+++ client/src/components/BookCard/BookCard.js
@@ -3,13 +3,21 @@ import './BookCard.css';
 
 const BookCard = (props) => {
   return (
-    <div className="card-container">
+    <div className="row">
+      <div className="leftcolumn">
+    <div className="card bg-dark text-white">
       <img src={props.image} alt="" />
-      <div className="desc">
+      <div className="card bg-dark text-white">
         <h2>{props.title}</h2>
+        <br/>
         <h3>Author: {props.author}</h3>
+        <br/>
         <p>Published Date: {props.published === '0000' ? 'Not available': props.published.substring(0,4)}</p>
+        <br/>
+        <p>Description: {props.description}</p>
       </div>
+    </div>
+    </div>
     </div>
   );
 };
--- client/src/components/BookList/BookList.js
+++ client/src/components/BookList/BookList.js
@@ -14,6 +14,7 @@ const BookList = (props) => {
                             title={book.volumeInfo.title}
                             author={book.volumeInfo.authors}
                             published={book.volumeInfo.publishedDate}
+                            description={book.volumeInfo.description}
                             />
             })
         }

f212c0147784d5ccf8376a921b9ae4a6d4b8fed2
david mendoza <davidsmacbook@davids-mbp.attlocal.net>
2020-12-05 07:15:36 -0800

    applied sorting

--- client/src/components/Books/Books.js
+++ client/src/components/Books/Books.js
@@ -21,7 +21,10 @@ class Books extends Component {
     .query({q: this.state.searchField})
     .then((data) => {
       console.log(data);
-      this.setState({ books : [...data.body.items]})
+      const cleanData = this.cleanData(data)
+      
+
+      this.setState({ books: cleanData})
     })
   }
 
@@ -33,11 +36,40 @@ class Books extends Component {
     this.setState({ sort: e.target.value })
   }
 
+  cleanData = (data) => {
+    const cleanedData = data.body.items.map((book) => {
+      if (book.volumeInfo.hasOwnProperty('publishedDate') === false) {
+        book.volumeInfo['publishedDate'] ='0000';
+      }
+
+      else if(book.volumeInfo.hasOwnProperty('imageLinks') === false) {
+        book.volumeInfo['imageLinks'] = {thumbnail: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/480px-No_image_available.svg.png" }
+      }
+
+      return book;
+    })
+
+    return cleanedData;
+    }
+
+
+
   render() {
+    const sortedBooks = this.state.books.sort((a, b) => {
+      if(this.state.sort === 'Newest') {
+        return parseInt(b.volumeInfo.publishedDate.substring(0,4)) - parseInt(a.volumeInfo.publishedDate.substring(0,4))
+      }
+      else  if(this.state.sort === 'Oldest') {
+        return parseInt(a.volumeInfo.publishedDate.substring(0,4)) - parseInt(b.volumeInfo.publishedDate.substring(0,4))
+      }
+
+      
+    })
+
     return (
       <div>
         <SearchArea searchBook={this.searchBook} handleSearch={this.handleSearch} handleSort={this.handleSort} />
-        <BookList books={this.state.books}/>
+        <BookList books={sortedBooks}/>
       </div>
     );
   }
